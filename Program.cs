b56c55dd5ac8d18d38708a6315be0253f3da2884
ElAiwan <chavarriaivan97@gmail.com>
2024-08-21 10:59:20 -0600

    Ejercicio para saludar a unas persona con C#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace HolaMundo
+﻿using System;
+
+namespace HolaMundo
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            // Ejercicio 1: Hola Mundo
             Console.WriteLine("Hello, World!");
+
+            // Ejercicio 2: Saludar a una persona
+            Console.WriteLine("Por favor, ingrese su nombre:");
+            string nombre =  Console.ReadLine();
+            Console.WriteLine("Hola, " + nombre + "!");
+
         }
     }
 }
