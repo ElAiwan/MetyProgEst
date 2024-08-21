e0d3c20e08a2620bea26eb16b7dfd2a25e3f281b
ElAiwan <chavarriaivan97@gmail.com>
2024-08-21 11:35:54 -0600

    Ejercicio para sumar dos número enteros, segunda variante y comentarios

--- Program.cs
+++ Program.cs
@@ -15,15 +15,23 @@ namespace HolaMundo
             Console.WriteLine("Hola, " + nombre + "!");
 
             // Ejercicio 3: Suma de dos números
-            Console.WriteLine("Ingresa un número:");
-            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa un número:");        // Solicita al usuario que ingrese un número
+            int numero1 = int.Parse(Console.ReadLine());    // Convierte la entrada de texto del usuario en un número entero y lo asigna a la variable numero1
 
-            Console.WriteLine("Ingresa otro número:");
-            int numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa otro número:");      // Solicita al usuario que ingrese un número
+            int numero2 = int.Parse(Console.ReadLine());    // Convierte la entrada de texto del usuario en un número entero y lo asigna a la variable numero2
 
-            int suma = numero1 + numero2;
+            int suma = numero1 + numero2;   // Suma los dos números y almacena el resultado en la variable suma
 
-            Console.WriteLine("La suma de " + numero1 + " y " + numero2 + " es " + suma);
+            Console.WriteLine("La suma de " + numero1 + " y " + numero2 + " es " + suma);   // Imprime el resultado de la suma al usuario
+
+            //Ejercicio 3: Suma de dos números - segunda variante
+            // Console.WriteLine("Ingresa un número:"); este código en este segunda variante del ejercicio es innecesario, ya que las variantes son definidas en el código
+            int a = 7;  // primera variable definida, no se puede cambiar
+            int b = 8;  // variable variable definida, no se puede cambiar
+            int c = a + b;  // Suma las dos variables fijas y almacena el resultado en la variable c
+
+            Console.WriteLine("La suma de " + a + " y " + b + " es: " +c); // Imprime el resultado de la suma al usuario
         }
     }
 }
